3700d1340161840df5d734bc6251e64828b18380
Ronaldo Cac <alexj@balboadigital.com>
2023-10-20 00:20:52 +0800

    Vercel Update

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),  
-]
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
